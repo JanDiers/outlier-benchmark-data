693f4e59687648534336ec1cc1af44fa33801df0
JanDiers <jan.diers@uni-jena.de>
2021-01-22 12:22:02 +0100

    changed implementation for calling callbacks

--- outlier_benchmark/datasets/base/dataset.py
+++ outlier_benchmark/datasets/base/dataset.py
@@ -10,41 +10,26 @@ import pandas as pd
 from outlier_benchmark.callbacks.base.callback import BaseCallback
 
 
-def _callbacks_on(dataset_method: str):
-    """
-    This function serves as decorator for dataset methods. It ensures:
-    - callback.before_{dataset_method} is called
-    - dataset.{method} is called
-    - callback.after_{dataset_method} is called.
+def load_callbacks(load):
+    @functools.wraps(load)
+    def call_load(*args, **kwargs):
+        dataset: BaseDataset = args[0]
 
-    The results are returned.
+        # execute callbacks before load
+        for callback in dataset.callbacks:
+            if hasattr(callback, 'before_load'):
+                dataset = callback.before_load(dataset)
 
-    :param dataset_method: name of the dataset method, e.g. 'load'
-    :return: decorated method
-    """
-    def decorated_method(func):
-        @functools.wraps(func)
-        def wrap_decorated(*args, **kwargs):
-            dataset: BaseDataset = args[0]
+        X, y = load(*args, **kwargs)
 
-            # execute callbacks before method
-            for callback in dataset.callbacks:
-                if hasattr(callback, f'before_{dataset_method}'):
-                    method = getattr(callback, f'before_{dataset_method}')
-                    method(dataset)
+        # execute callbacks before load
+        for callback in dataset.callbacks:
+            if hasattr(callback, 'after_load'):
+                dataset, X, y = callback.after_load(dataset, X, y)
 
-            # execute method
-            value = func(*args, **kwargs)
+        return dataset, X, y
 
-            # execute callbacks after method
-            for callback in dataset.callbacks:
-                if hasattr(callback, f'after_{dataset_method}'):
-                    method = getattr(callback, f'after_{dataset_method}')
-                    method(dataset, *value)
-
-            return value
-        return wrap_decorated
-    return decorated_method
+    return call_load
 
 
 @dataclass
@@ -66,7 +51,7 @@ class BaseDataset:
     def __post_init__(self):
         self.pct_outlier = round((self.num_outlier / self.num_samples) * 100, 2)
 
-    @_callbacks_on('load')
+    @load_callbacks
     def load(self, download: bool = True) -> Tuple[np.ndarray, np.ndarray]:
         """
         loads the data X and y, both numpy arrays. If not previously downloaded and ``download=True``, before loading
